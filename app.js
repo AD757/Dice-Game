14d649f9612920229b6a63434efb25656dffc5fa
Alladin Daher <alladin.daher@hotmail.com>
2021-01-04 21:29:13 +0100

    Update app.js
    removed comments

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 'use strict';
 
-// selecting elements
 const score0 = document.querySelector('#score--0');
 const score1 = document.querySelector('#score--1');
 const player0 = document.querySelector('.player--0');
@@ -15,7 +14,6 @@ let currentScore, activePlayer, playing, scores;
 
 initialValues();
 
-// Initial scores
 function initialValues() {
   scores = [0, 0];
   currentScore = 0;
@@ -35,7 +33,6 @@ function initialValues() {
   document.getElementById('name--1').textContent = 'Player 2';
 }
 
-// Switch active player
 const switchActivePlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
   currentScore = 0;
@@ -49,40 +46,31 @@ const switchActivePlayer = function () {
   diceEl.classList.add('hidden');
 };
 
-// New Player button
 btnNew.addEventListener('click', initialValues);
 
-// Roll dice button
 btnRoll.addEventListener('click', function () {
   if (playing) {
-    // Generate dice
     let dice = Math.trunc(Math.random() * 6) + 1;
-    // Based on dice value show the dice image
     diceEl.classList.remove('hidden');
     diceEl.src = `dice-${dice}.png`;
-    // If 1 is not rolled, add dice to current score
     if (dice !== 1) {
       currentScore += dice;
       document.getElementById(
         `current--${activePlayer}`
       ).textContent = currentScore;
     }
-    // If 1 is rolled -- make current score to 0, Switch player
     else {
       switchActivePlayer();
     }
   }
 });
 
-// Hold button
 btnHold.addEventListener('click', function () {
   if (playing) {
-    // Add current score to player score and display it
     scores[activePlayer] += currentScore;
     document.querySelector(`#score--${activePlayer}`).textContent =
       scores[activePlayer];
 
-    // If player score = 100, Winner and stop game
     if (scores[activePlayer] >= 100) {
       playing = false;
       document
@@ -95,12 +83,10 @@ btnHold.addEventListener('click', function () {
       document.getElementById(`name--${activePlayer}`).textContent =
         'WINNER!🥳 ';
     }
-    // If not, switchActivePlayer()
     else {
       switchActivePlayer();
     }
   }
 
-  // Restart - New Game
   btnNew.addEventListener('click', init);
 });
